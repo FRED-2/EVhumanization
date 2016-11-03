280133bff534b5722e30b8936f67672115f86723
sophiamersmann <sophia.mersmann1@gmail.com>
2016-11-03 16:54:49 +0100

    applied PEP8 styleguide

--- deimmunization/grafting.py
+++ deimmunization/grafting.py
@@ -18,16 +18,25 @@ from Bio.SubsMat import MatrixInfo as matlist
 from kabat_numbering import KabatNumbering
 from contacts_from_structure import download_pdb_file, make_contact_map
 
+
 class Grafting(object):
-    """Abstract class for grafting a subset of residues of the sequence to be humanized onto the human template sequence.
+    """Abstract class for grafting a subset of residues of the sequence to be
+    humanized onto the human template sequence.
 
     Attributes:
         sequence -- SeqRecord of the sequence to be humanized
-        template -- SeqRecord of the human sequence serving as a template in the humanization process
-        mapping -- mapping of sequence to template as dictionary: key = residue number of sequence, value = residue number of template
+        template -- SeqRecord of the human sequence serving as a template
+                    in the humanization process
+        mapping -- mapping of sequence to template as dictionary:
+                   key = residue number of sequence,
+                   value = residue number of template
                    (residues not zero-based numbered, i.e. starting from 1)
-        residues_to_graft -- list of residues of the sequence to be grafted onto the template (needs to be set in inheriting class)
-        humanized_seq -- SeqRecord of the humanized sequence, i.e. resulting sequence by grafting residues_to_graft onto template
+        residues_to_graft -- list of residues of the sequence to be grafted
+                             onto the template
+                             (needs to be set in inheriting class)
+        humanized_seq -- SeqRecord of the humanized sequence,
+                         i.e. resulting sequence by grafting residues_to_graft
+                         onto template
     """
 
     __metaclass__ = ABCMeta
@@ -43,17 +52,23 @@ class Grafting(object):
 
     @abstractmethod
     def find_residues_to_graft():
-        """Find residues of sequence to graft onto template and fill attribute residues_to_graft accordingly."""
+        """
+            Find residues of sequence to graft onto template and fill attribute
+            residues_to_graft accordingly.
+        """
         pass
 
     def map_sequence_to_template(self, gap_open=-11, gap_extend=-1):
         """Map sequence to be humanized to human template sequence.
 
         Args:
-            gap_open: penalty for introducing a gap in alignment calculation (default: -11)
-            gap_extend: penalty for extending a gapped section in alignment calculation (default: -1)
+            gap_open: penalty for introducing a gap in alignment calculation
+                      (default: -11)
+            gap_extend: penalty for extending a gapped section in alignment
+                        calculation (default: -1)
         """
-        aln_sequence,aln_template,_,_,_  = pairwise2.align.globalds(self.sequence.seq.upper(), self.template.seq.upper(),
+        aln_sequence, aln_template, _, _, _ = pairwise2.align.globalds(
+            self.sequence.seq.upper(), self.template.seq.upper(),
             matlist.blosum62, gap_open, gap_extend)[0]
         self.mapping = defaultdict(lambda: -1)
         seq_res_num, tem_res_num = 1, 1
@@ -68,13 +83,17 @@ class Grafting(object):
                 seq_res_num += 1
 
     def graft(self):
-        """Graft residues_to_graft onto template and save SeqRecord of the resulting humanized sequence."""
+        """
+            Graft residues_to_graft onto template and save SeqRecord of the
+            resulting humanized sequence.
+        """
         mod_seq = list(self.template.seq)
         for res_num in self.residues_to_graft:
             tem_res_num = self.mapping[res_num]
             if tem_res_num != -1:
                 mod_seq[tem_res_num-1] = self.sequence.seq[res_num-1]
-        self.humanized_seq = SeqRecord(seq=Seq(''.join(mod_seq), SingleLetterAlphabet()),
+        self.humanized_seq = SeqRecord(
+            seq=Seq(''.join(mod_seq), SingleLetterAlphabet()),
             id='humanized_%s' % self.sequence.id,
             name='humanized %s' % self.sequence.id,
             description='%s humanized by %s' % (self.sequence.id, self.template.id))
@@ -97,16 +116,16 @@ class CDRGrafting(Grafting):
     """
 
     # CDRs of light chain defined by Kabat
-    L_1 = ((24,''),(34,''))
-    L_2 = ((50,''),(56,''))
-    L_3 = ((89,''),(97,''))
+    L_1 = ((24, ''), (34, ''))
+    L_2 = ((50, ''), (56, ''))
+    L_3 = ((89, ''), (97, ''))
 
     # CDRs of heavy chain defined by Kabat
-    H_1 = ((31,''),(35,'B'))
-    H_2 = ((50,''),(65,''))
-    H_3 = ((95,''),(102,''))
+    H_1 = ((31, ''), (35, 'B'))
+    H_2 = ((50, ''), (65, ''))
+    H_3 = ((95, ''), (102, ''))
 
-    _ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    _ALPHABET = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
     def __init__(self, sequence, template):
         super(CDRGrafting, self).__init__(sequence, template)
@@ -115,21 +134,25 @@ class CDRGrafting(Grafting):
         self.graft()
 
     def find_residues_to_graft(self):
-        """Find residues at the CDRs of the sequence to be humanized and save the found residue numbers in residues_to_graft."""
+        """
+            Find residues at the CDRs of the sequence to be humanized and save
+            the found residue numbers in residues_to_graft.
+        """
         mode = self.kabat_seq.kabat_list[0][0][0]
         loop_regions = [CDRGrafting.L_1, CDRGrafting.L_2, CDRGrafting.L_3]\
             if mode == 'L' else [CDRGrafting.H_1, CDRGrafting.H_2, CDRGrafting.H_3]
         self.residues_to_graft = []
         for b, e in loop_regions:
             for i in xrange(b[0], e[0]+1):
-                kabat = (i,'')
+                kabat = (i, '')
                 if kabat in self.kabat_seq.kabat_dict:
                     self.residues_to_graft.append(self.kabat_seq.kabat_dict[kabat])
                 search_alpha = CDRGrafting._ALPHABET
                 if i == e[0]:
-                    search_alpha = CDRGrafting._ALPHABET[:operator.indexOf(CDRGrafting._ALPHABET,e[1])+1] if e[1] != '' else []
+                    search_alpha = CDRGrafting._ALPHABET[:operator.indexOf(CDRGrafting._ALPHABET, e[1])+1]\
+                        if e[1] != '' else []
                 for letter in search_alpha:
-                    kabat = (i,letter)
+                    kabat = (i, letter)
                     if kabat in self.kabat_seq.kabat_dict:
                         self.residues_to_graft.append(self.kabat_seq.kabat_dict[kabat])
 
@@ -147,7 +170,9 @@ class SDRGrafting(Grafting):
     def make_contact_map_wrapper(pdb_id, chains):
         pdb_file_name = pdb_id + '.pdb'
         download_pdb_file(pdb_id, pdb_file_name)
-        pair_dist_lists = make_contact_map(pdb_id, list(chains), pdb_file_name=pdb_file_name, use_sifts_mapping=True)
+        pair_dist_lists = make_contact_map(pdb_id, list(chains),
+                                           pdb_file_name=pdb_file_name,
+                                           use_sifts_mapping=True)
         system('rm ' + pdb_file_name)
         return pair_dist_lists[tuple(chains)]
 
@@ -157,7 +182,9 @@ class SDRGrafting(Grafting):
 
 
 class ManualGrafting(Grafting):
-    """Humanize sequence by grafting of manually specified non-human residues onto the human template sequence."""
+    """Humanize sequence by grafting of manually specified non-human residues
+    onto the human template sequence.
+    """
 
     def __init__(self, sequence, template, given_residues):
         super(ManualGrafting, self).__init__(sequence, template)
@@ -167,33 +194,34 @@ class ManualGrafting(Grafting):
     def find_residues_to_graft(self, given_residues):
         self.residues_to_graft = [int(res_num) for res_num in given_residues.split(',')]
 
+
 def command_line():
     parser = argparse.ArgumentParser(description='Initial humanization of an antibody by CDR or SDR Grafting')
     parser.add_argument('--sequence', '-s',
-        required=True,
-        help='The sequence to be humanized (in fasta format)')
+                        required=True,
+                        help='The sequence to be humanized (in fasta format)')
     parser.add_argument('--template', '-t',
-        required=True,
-        help='The human sequence to be used as a template (in fasta format)')
+                        required=True,
+                        help='The human sequence to be used as a template (in fasta format)')
     parser.add_argument('--pdb_id', '-p',
-        required=False,
-        help='PDB ID for using structure to determine antigen contacts of the murine antibody domain '
-            +'(default: use CDRs defined by Kabat)')
+                        required=False,
+                        help='PDB ID for using structure to determine antigen contacts of the murine antibody domain '
+                            +'(default: use CDRs defined by Kabat)')
     parser.add_argument('--chains', '-c',
-        required=False,
-        help='Chains of antiboy domain to be humanized (first) and of its cognate antigen (second), '
-            +'e.g. DB (D: antibody chain, B: antigen chain)')
+                        required=False,
+                        help='Chains of antiboy domain to be humanized (first) and of its cognate antigen (second), '
+                            +'e.g. DB (D: antibody chain, B: antigen chain)')
     parser.add_argument('--manual', '-m',
-        required=False,
-        help='The non-human residues to be grafted onto the human template sequence, '
-            +'specified by a comma-separated list of residue numbers, e.g. 14,15,16,79,80 '
-            +'(residue numbers starting from 1)')
+                        required=False,
+                        help='The non-human residues to be grafted onto the human template sequence, '
+                            +'specified by a comma-separated list of residue numbers, e.g. 14,15,16,79,80 '
+                            +'(residue numbers starting from 1)')
     parser.add_argument('--output', '-o',
-        required=True,
-        help='Output File')
+                        required=True,
+                        help='Output File')
     args = parser.parse_args()
 
-    required_together = [getattr(args,x) for x in ('pdb_id', 'chains')]
+    required_together = [getattr(args, x) for x in ('pdb_id', 'chains')]
     if not all(required_together) and not required_together == 2*[None]:
         print >> stderr, 'If structural information is used, both pdb id and chains must be specified.'
         sys.exit(-1)
